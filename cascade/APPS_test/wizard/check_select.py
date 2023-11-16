59f15f8d6dcb410c155556c13aec0f341caf86bf
BoyuanJackChen <bc3194@nyu.edu>
2023-11-16 14:19:45 -0500

    APPS needs work

--- cascade/APPS_test/wizard/check_select.py
+++ cascade/APPS_test/wizard/check_select.py
@@ -3,13 +3,13 @@ import numpy as np
 import pandas as pd
 import multiprocessing
 import os
-from datasets import load_dataset
 
+# Create files in selected
 num_loops = 10
 pick_at = 10
 all_limit_lines = [2,4]
-all_actual_pick_at = [1,3,5,10]
-model_name = "7B"
+all_actual_pick_at = [0,1,3,5,10]
+model_name = "34B"
 all_accuracies = np.zeros(num_loops)
 import_lines = "import math\nfrom typing import List, Tuple\n"
 all_questions_num = list(range(4000,5000))
@@ -42,7 +42,11 @@ def find_max_product(matrix):
 for limit_lines in all_limit_lines:
     for actual_pick_at in all_actual_pick_at:
         for loop in range(num_loops):
-            if actual_pick_at == 1:
+            if actual_pick_at == 0:
+                answer_file = f"./answer/{model_name}/{model_name}_p0_l0.json"
+                testcase_file = f"./testcase/{model_name}/{model_name}_p0_l0.json"
+                selected_file = f"./selected/{model_name}/{model_name}_p0_t0_l0.json"
+            elif actual_pick_at == 1:
                 answer_file = f"./answer/{model_name}/{model_name}_p0_l0.json"
                 testcase_file = f"./testcase/{model_name}/{model_name}_p0_l0.json"
                 selected_file = f"./selected/{model_name}/{model_name}_p1_t{limit_lines}_l0.json"
@@ -62,13 +66,17 @@ for limit_lines in all_limit_lines:
                 testcase_data = json.load(f)
 
             for number in all_questions_num:
+                # Collect the number of ids, so we can calculate time
+                num_ids = 0
+                
                 # Collect all answers for this question
                 all_answers = []
-                answers_pick_at = actual_pick_at + 0
+                answers_pick_at = max(actual_pick_at + 0, 1)   # We still want 1 answer when k=0
                 for answer_dict in answer_data:
                     if answer_dict["number"]==number and answers_pick_at>0:
                         answer = answer_dict["answer"]
                         all_answers.append(answer)
+                        num_ids += answer_dict["num_ids"]
                         answers_pick_at -= 1
                 
                 # Collect all tests for this question
@@ -81,12 +89,26 @@ for limit_lines in all_limit_lines:
                         for j in range(min(limit_lines, len(testlines))):
                             if testlines[j].startswith("assert"):
                                 all_generated_tests.append(testlines[j])
+                        num_ids += testcase_dict[f"num_ids_{limit_lines}"]
                         tests_pick_at -= 1
                 
-                # Initiate the correctness stats
+                # No need to check if k=0, since there is no testcase
+                if actual_pick_at == 0:
+                    selected_dict = {
+                        "number": number,
+                        "max_answer_num": 0,
+                        "max_test_num": 0,
+                        "total_product": 0,
+                        "answer": all_answers[0],
+                        "test": "",
+                        "num_ids": int(num_ids),
+                    }
+                    all_selected.append(selected_dict)
+                    print(f"Question {number}: Max product: 0")
+                    continue
+
+                # Check the correctness for each answer-testline
                 correct_stats = np.zeros([len(all_answers),len(all_generated_tests)], np.int32)
-                
-                # Check the correctness for each combination
                 def code_to_run(a, t, import_lines, answer, generated_test, result_queue):
                     full_code = import_lines + answer + "\n" + generated_test
                     try:
@@ -96,8 +118,6 @@ for limit_lines in all_limit_lines:
                         result_queue.put((a, t, False))
                 processes = []
                 result_queue = multiprocessing.Queue()
-                
-                # Start all processes without waiting for them to complete
                 for a in range(len(all_answers)):
                     for t in range(len(all_generated_tests)):
                         answer = all_answers[a]
@@ -105,21 +125,15 @@ for limit_lines in all_limit_lines:
                         process = multiprocessing.Process(target=code_to_run, args=(a, t, import_lines, answer, generated_test, result_queue))
                         process.start()
                         processes.append(process)
-
-                # Impose a 1-second time limit on each process
                 for process in processes:
                     process.join(1)  # Kill infinite loops in 1 second
                     if process.is_alive():
                         process.terminate()
                         process.join()
-
-                # After all processes are done or terminated, retrieve results from the queue
                 while not result_queue.empty():
                     a, t, correct = result_queue.get()
                     if correct:
                         correct_stats[a][t] += 1
-
-                # Close all processes
                 for process in processes:
                     process.close()
                 
@@ -132,11 +146,11 @@ for limit_lines in all_limit_lines:
                     "max_test_num": int(max_test_num),
                     "total_product": int(len(all_answers)*len(all_generated_tests)),
                     "answer": selected_answer,
-                    "test": selected_test
+                    "test": selected_test,
+                    "num_ids": int(num_ids),
                 }
                 all_selected.append(selected_dict)
                 print(f"Question {number}: Max product: {max_product}; indices: {indices}, ({max_answer_num}, {max_test_num})")
-                # print(correct_stats)
 
             # Write to file
             with open(selected_file, 'w') as f:
